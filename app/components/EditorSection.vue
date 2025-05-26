0c9956d155c3835caf1509d406a312bb1db8bb09
VitroidFPV <76877124+VitroidFPV@users.noreply.github.com>
2025-05-26 12:33:25 +0200

    Feature: Introduce tabs for character and image editing

--- app/components/EditorSection.vue
+++ app/components/EditorSection.vue
@@ -4,6 +4,8 @@ import { useOptionsStore } from "~~/stores/optionsStore"
 import { usePaletteStore } from "~~/stores/paletteStore"
 import { useHistoryStore } from "~~/stores/historyStore"
 
+import type { TabsItem } from "@nuxt/ui"
+
 const fontStore = useFontStore()
 const optionsStore = useOptionsStore()
 const paletteStore = usePaletteStore()
@@ -22,105 +24,157 @@ const handleScroll = (event: Event) => {
 		}
 	}
 }
+
+const tabs = [
+	{
+		label: "Painting Tools",
+		icon: "i-heroicons-pencil-square",
+		slot: "character-editor" as const
+	},
+	{
+		label: "Image Tools",
+		icon: "i-heroicons-image",
+		slot: "image-editor" as const
+	}
+] satisfies TabsItem[]
 </script>
 
 <template>
 	<h2 class="text-2xl font-bold text-primary-400">Character Preview</h2>
-	<template v-if="fontStore.hasData">
-		<div class="flex flex-col gap-2">
-			<div class="flex items-center gap-2">
-				<label for="characterIndex">Character Index</label>
-				<UInput
-					id="characterIndex"
-					v-model="fontStore.selectedCharacterIndex"
-					type="number"
-					:ui="{
-						base: 'w-20'
-					}"
-					@wheel="handleScroll"
-				/>
-				<span class="text-neutral-400 font-mono text-xs">
-					(Hex:
-					{{ fontStore.selectedCharacterIndex.toString(16).toUpperCase() }})
-				</span>
-			</div>
-		</div>
-		<div class="flex flex-col gap-4 w-fit">
-			<CharacterGrid
-				:show-grid="optionsStore.showGrid"
-				:show-background="optionsStore.showBackground"
-				:show-tooltip="optionsStore.showTooltip"
-			/>
-			<div class="flex items-center justify-between w-full">
-				<div class="flex items-center gap-2">
-					<UButton
-						:color="paletteStore.selectedColor === 0 ? 'primary' : 'neutral'"
-						:variant="paletteStore.selectedColor === 0 ? 'solid' : 'soft'"
-						:ui="{ base: 'p-0.5' }"
-						@click="paletteStore.selectedColor = 0"
-					>
-						<div class="aspect-square w-8 bg-black rounded-sm"></div>
-					</UButton>
-					<UButton
-						:color="paletteStore.selectedColor === 2 ? 'primary' : 'neutral'"
-						:variant="paletteStore.selectedColor === 2 ? 'solid' : 'soft'"
-						:ui="{ base: 'p-0.5' }"
-						@click="paletteStore.selectedColor = 2"
-					>
-						<div class="aspect-square w-8 bg-white rounded-sm"></div>
-					</UButton>
-					<UButton
-						:color="paletteStore.selectedColor === 3 ? 'primary' : 'neutral'"
-						:variant="paletteStore.selectedColor === 3 ? 'solid' : 'soft'"
-						:ui="{ base: 'p-0.5' }"
-						@click="paletteStore.selectedColor = 3"
-					>
-						<div class="aspect-square w-8 bg-neutral-400 rounded-sm"></div>
-					</UButton>
+
+	<UTabs
+		v-if="fontStore.hasData"
+		:items="tabs"
+		variant="link"
+		class="gap-4 w-full"
+		:ui="{
+			trigger: 'grow'
+		}"
+	>
+		<template #character-editor>
+			<div class="flex flex-col gap-4">
+				<div class="flex gap-4">
+					<div class="flex flex-col gap-4 w-fit">
+						<CharacterGrid
+							:show-grid="optionsStore.showGrid"
+							:show-background="optionsStore.showBackground"
+							:show-tooltip="optionsStore.showTooltip"
+						/>
+						<div class="flex items-center justify-between w-full">
+							<div class="flex items-center gap-2">
+								<UButton
+									:color="
+										paletteStore.selectedColor === 0 ? 'primary' : 'neutral'
+									"
+									:variant="paletteStore.selectedColor === 0 ? 'solid' : 'soft'"
+									:ui="{ base: 'p-0.5' }"
+									@click="paletteStore.selectedColor = 0"
+								>
+									<div class="aspect-square w-8 bg-black rounded-sm"></div>
+								</UButton>
+								<UButton
+									:color="
+										paletteStore.selectedColor === 2 ? 'primary' : 'neutral'
+									"
+									:variant="paletteStore.selectedColor === 2 ? 'solid' : 'soft'"
+									:ui="{ base: 'p-0.5' }"
+									@click="paletteStore.selectedColor = 2"
+								>
+									<div class="aspect-square w-8 bg-white rounded-sm"></div>
+								</UButton>
+								<UButton
+									:color="
+										paletteStore.selectedColor === 3 ? 'primary' : 'neutral'
+									"
+									:variant="paletteStore.selectedColor === 3 ? 'solid' : 'soft'"
+									:ui="{ base: 'p-0.5' }"
+									@click="paletteStore.selectedColor = 3"
+								>
+									<div
+										class="aspect-square w-8 bg-neutral-400 rounded-sm"
+									></div>
+								</UButton>
+							</div>
+							<div class="flex items-center gap-2">
+								<UButton
+									:disabled="!historyStore.canUndo"
+									icon="i-heroicons-arrow-uturn-left"
+									color="neutral"
+									variant="subtle"
+									size="sm"
+									class="rounded-full"
+									:tooltip="{
+										text: 'Undo (Ctrl+Z)',
+										disabled: !historyStore.canUndo
+									}"
+									@click="historyStore.undo()"
+								/>
+								<UButton
+									:disabled="!historyStore.canRedo"
+									icon="i-heroicons-arrow-uturn-right"
+									color="neutral"
+									variant="subtle"
+									size="sm"
+									class="rounded-full"
+									:tooltip="{
+										text: 'Redo (Ctrl+Y)',
+										disabled: !historyStore.canRedo
+									}"
+									@click="historyStore.redo()"
+								/>
+							</div>
+						</div>
+						<div class="flex items-center w-full justify-between">
+							<div class="flex items-center gap-2">
+								<label for="characterIndex">Character Index</label>
+								<UInput
+									id="characterIndex"
+									v-model="fontStore.selectedCharacterIndex"
+									type="number"
+									:ui="{
+										base: 'w-20'
+									}"
+									@wheel="handleScroll"
+								/>
+							</div>
+							<span class="text-neutral-400 font-mono text-xs">
+								(Hex:
+								{{
+									fontStore.selectedCharacterIndex.toString(16).toUpperCase()
+								}})
+							</span>
+						</div>
+					</div>
+					<div class="flex flex-col gap-2 text-text text-sm max-w-[50ch]">
+						<p>
+							Select a character to edit, either from the font preview or from
+							the input field. Enter in a specific index, or use the scroll
+							wheel to navigate through the character set.
+						</p>
+						<p>Select a color to paint pixels.</p>
+						<p>
+							<UKbd>Left-click</UKbd> on a pixel to change its color.
+							<UKbd>Right-click</UKbd> on a pixel to clear it (set to
+							transparent). Hold mouse button to paint a continuous area.
+						</p>
+						<p>
+							<UKbd>Ctrl</UKbd> + <UKbd>Z</UKbd> to undo, <UKbd>Ctrl</UKbd> +
+							<UKbd>Y</UKbd> to redo.
+						</p>
+						<p>
+							<span class="text-dimmed">
+								Gray pixels are only visible when using specific OSD renderers.
+								They will show as transparent in other renderers.
+							</span>
+						</p>
+					</div>
 				</div>
-				<div class="flex items-center gap-2">
-					<UButton
-						:disabled="!historyStore.canUndo"
-						icon="i-heroicons-arrow-uturn-left"
-						color="neutral"
-						variant="subtle"
-						size="sm"
-						class="rounded-full"
-						:tooltip="{
-							text: 'Undo (Ctrl+Z)',
-							disabled: !historyStore.canUndo
-						}"
-						@click="historyStore.undo()"
-					/>
-					<UButton
-						:disabled="!historyStore.canRedo"
-						icon="i-heroicons-arrow-uturn-right"
-						color="neutral"
-						variant="subtle"
-						size="sm"
-						class="rounded-full"
-						:tooltip="{
-							text: 'Redo (Ctrl+Y)',
-							disabled: !historyStore.canRedo
-						}"
-						@click="historyStore.redo()"
-					/>
-				</div>
 			</div>
-		</div>
-		<p class="text-text text-sm space-y-1">
-			Select a color to paint pixels.
-			<br />
-			Left-click on a pixel to change its color.
-			<br />
-			Right-click on a pixel to clear it (set to transparent).
-			<br />
-			<span class="text-xs text-dimmed">
-				Gray pixels are only visible when using specific OSD renderers. They
-				will show as transparent in other renderers.
-			</span>
-		</p>
-	</template>
+		</template>
+		<template #image-editor>
+			<div>Image Editor</div>
+		</template>
+	</UTabs>
 	<div
 		v-else
 		class="flex items-center justify-center h-64 text-neutral-400 text-center font-mono"
